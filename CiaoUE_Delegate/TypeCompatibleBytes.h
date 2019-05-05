61426e6a52e8c6ec67d52e320dfc63dc6927f108
CiaoLee1996 <1020286288@qq.com>
2019-05-05 23:27:35 +0800

    Modified TypeCompatibleBytes.h

--- CiaoUE_Delegate/TypeCompatibleBytes.h
+++ CiaoUE_Delegate/TypeCompatibleBytes.h
@@ -1,5 +1,29 @@
 #pragma once
 
+
+//define GCC_ALIGN and  MS_ALIGN as empty.
+#ifndef  GCC_ALIGN
+#define GCC_ALIGN(n) 
+#endif
+
+#ifndef  MS_ALIGN
+#define MS_ALIGN(n)
+#endif 
+
+#if defined(__clang__)
+//With -fms_extensions. Clang will accept either alignment attribute.
+#if defined(_MSC_VER)
+#define MS_ALIGN(n) __declspec(align(n))
+#endif
+#endif
+#else
+#define MS_ALIGN(n) __declspec(align(n))
+#endif
+
+#if  !defined(__clang__)
+#define MSVC_PRAGMA( Pragma) __pragma(Pragma)
+#endif
+
 /**
  *  Used to declare an untyped array of data with compile-time alignment.
  *  It needs to use template specialization as the MS_ALIGN and GCC_ALIGN macros require literal parameters.
@@ -14,9 +38,19 @@ struct TAlignedBytes<Bytes, 1>
 	unsigned char Pad[Size];
 };
 
-//Ignore padding and alignment implementation.
+#define IMPLEMENT_ALIGNED_STORAGE(Align)	\
+template<int Size>	\
+struct TAlignedBytes<Size,Align>	\
+{	\
+	//Define structure TPadding.
+	struct TPadding	\ MS_ALIGN(Align)
+	{	\
+		unsigned char Pad[Size]; \
+	};  GCC_ALIGN(Align)\
 
-//End Ignore;
+	TPadding Padding; \
+};
+
 
 
 //An untyped array of data with compile-time alignment and size derived from another type.
