7434e4cec813f9c27c7498cf0676a0369bc6d067
ciaoli <ciaoli@git.code.tencent.com>
2019-05-07 21:06:12 +0800

    Continue add TRawMethodDelegateInstance.

--- CiaoUE_Delegate/DelegateInstancesImpl.h
+++ CiaoUE_Delegate/DelegateInstancesImpl.h
@@ -2,6 +2,7 @@
 
 #include <tuple>
 #include "IDelegateInstance.h"
+#include "DelegateInstanceInterface.h"
 
 class FDelegateBase;
 class FDelegateHandle;
@@ -11,9 +12,52 @@ template<bool bConst,class UserClass,typename FuncType,typename... VarTypes>
 class TBaseRawMethodDelegateInstance;
 
 template<bool bConst,class UserClass,typename WrappedRetValType, typename... ParamTypes, typename... VarTypes>
-class TBaseRawMethodDelegateInstance<bConst, UserClass, WrappedRetValType(ParamTypes...), VarTypes...>
+class TBaseRawMethodDelegateInstance<bConst, UserClass, WrappedRetValType(ParamTypes...), VarTypes...> : public IBaseDelegateInstance<typename TUnwrapType<WrappedRetValType>::Type (ParamTypes..)>
 {
+public:
+
+	//Get the RetValType.
+	typedef typename TUnwrapType<WrappedRetValType>::Type RetValType;
+
+private:
+
+	typedef IBaseDelegateInstance<RetValType(ParamTypes...)>                                          Super;
+	typedef TBaseRawMethodDelegateInstance<bConst, UserClass, RetValType(ParamTypes...), VarTypes...> UnwrappedThisType;
+
+public:
+	//Typedef the function return 
+	typedef typename TMemFunPtrType<bConst, UserClass, RetValType(ParamTypes..., VarTypes...)>::Type FMethodPtr;
+
+	//Create and initilaize a new instance;
+	TBaseRawMethodDelegateInstance(UserClass* InUserObject, FMethodPtr InMethodPtr, VarTypes... Vars)
+		:UserObject(InUserObject)
+		,MethodPtr(InMethodPtr)
+		,Payload(Vars...)
+		,Handle(FDelegateHandle::EGenerateNewHandleType::GenerateNewHandle)
+	{
+		//InUserObject and InMethodPtr could not be zero.
+	}
+
+	//Deprecated.
+	virtual bool HasSomeObject(const void* InUserObject) const override final
+	{
+		return UserObject == InUserObject;
+	}
 
 
+protected:
+
+	//Pointer to the user's class which contains a method we would like to call
+	UserClass* UserObject;
+
+	//C++ member function pointer.
+	FMethodPtr MethodPtr;
+
+	//Payload member variables(if any).
+	std::tuple<VarTypes....> Payload;
+
+	//The handle of this delegate
+	FDelegateHandle Handle;
 };
+
 
--- CiaoUE_Delegate/TemplateHelper.h
+++ CiaoUE_Delegate/TemplateHelper.h
@@ -1,6 +1,20 @@
 #pragma once
 
+//TypeWrapper;
+template<typename T>
+struct TTypeWrapper;
 
+template<typename T>
+struct TUnwrapType
+{
+	typedef T Type;
+};
+
+template<typename T>
+struct TUnwrapType<TTypeWrapper<T>>
+{
+	typedef  T  Type;
+};
 
 //TRemoveReference<type> will remove any references from a type.
 template<typename T> struct TRemoveReference
@@ -31,5 +45,3 @@ inline T&& Forward(typename TRemoveReference<T>::Type&& InObj)
 {
 	return (T&&)InObj;
 }
-
-//
